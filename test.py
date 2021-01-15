75ab93f19dce9545b1615c4ff2990323947c3820
iamrickt <rickttan@gmail.com>
2021-01-15 15:48:54 -0700

    added token

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 a = [1, 2, 3, 4, 5, ]
 
 print(len(a))
-<<<<<<< HEAD
-=======
+<< << << < HEAD
+== == == =
 
 
 print('new string in feature-c')
->>>>>>> feature-c
+>>>>>> > feature-c
+
+
+686a9c5c1fe038e0ea52254a74a5540bb27ee19b
